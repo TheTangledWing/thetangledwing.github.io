e1224c6178ad537284b133ccae706405092155ba
Alan Heraty <79844490+TheTangledWing@users.noreply.github.com>
2022-05-04 10:58:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = document.getElementById("main")
+ReactDOM.render(<h1>The Alan Heraty Experience  🚀</h1>, app)
+
+
+ReactDOM.render(<ul>
+    <li>News</li>
+    <li>Pics</li>
+    <li>Songs</li>
+    <li>Donate(I'm broke</li>
+</ul>, alist)
+
+
+
+
